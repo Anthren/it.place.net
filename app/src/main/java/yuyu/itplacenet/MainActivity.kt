544ae5c9e8f9bbee46fa073dd5e8ded7fc722dc9
silanteva.yu <borisova.yu.yu@gmail.com>
2018-02-14 18:17:28 +0400

    Added authorization

--- app/src/main/java/yuyu/itplacenet/MainActivity.kt
+++ app/src/main/java/yuyu/itplacenet/MainActivity.kt
@@ -2,11 +2,51 @@ package yuyu.itplacenet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import java.util.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.firebase.ui.auth.IdpResponse
+import android.content.Intent
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val providers = Arrays.asList(
+                AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build())
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+
+                if( user != null ) {
+                    Toast.makeText(this, user.toString(), Toast.LENGTH_SHORT ).show()
+                }
+            } else {
+                // Sign in failed, check response for error code
+                // ...
+            }
+        }
+    }
+
 }
